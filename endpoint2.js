709989bb718eefe088bc7634ca20ff8dee063ea1
matFS06 <131223153+matFS06@users.noreply.github.com>
2024-03-26 11:05:43 -0300

    Add files via upload

--- endpoint2.js
+++ endpoint2.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const app = express();
 const port = 3000; // porta
 
-// Configuração do banco de dados
+// ConfiguraÃ§Ã£o do banco de dados
 const pool = new Pool({
   user: 'coloar_usuario',
   host: 'localhost',
@@ -13,34 +13,51 @@ const pool = new Pool({
   port: 5432,
 });
 
-// Endpoint para listar a média de corte de um curso com base no turno, estado e nome do curso
+// Classe Repository para manipulaÃ§Ã£o de dados do banco de dados
+class CursoRepository {
+  async getMediaCorte(turno, estado, nome_curso) {
+    try {
+      const { rows } = await pool.query(
+        `
+        SELECT cc.media_minima
+        FROM Curso c
+        JOIN Concorrencia cc ON c.codigo_curso = cc.codigo_curso
+        WHERE c.turno = $1 AND c.estado = $2 AND c.nome_curso = $3
+        `,
+        [turno, estado, nome_curso]
+      );
+
+      if (rows.length === 0) {
+        return null; // Retorna null se o curso nÃ£o for encontrado
+      }
+
+      return rows[0].media_minima;
+    } catch (err) {
+      console.error('Erro ao consultar o banco de dados:', err);
+      throw new Error('Erro ao consultar o banco de dados');
+    }
+  }
+}
+
+const cursoRepository = new CursoRepository();
+
+// Endpoint para listar a mÃ©dia de corte de um curso com base no turno, estado e nome do curso
 app.get('/api/media_corte', async (req, res) => {
   const { turno, estado, nome_curso } = req.query;
 
   try {
-    const { rows } = await pool.query(
-      `
-      SELECT cc.media_minima
-      FROM Curso c
-      JOIN Concorrencia cc ON c.codigo_curso = cc.codigo_curso
-      WHERE c.turno = $1 AND c.estado = $2 AND c.nome_curso = $3
-      `,
-      [turno, estado, nome_curso]
-    );
+    const mediaCorte = await cursoRepository.getMediaCorte(turno, estado, nome_curso);
 
-    if (rows.length === 0) {
-      return res.status(404).json({ message: 'Curso não encontrado' });
+    if (!mediaCorte) {
+      return res.status(404).json({ message: 'Curso nÃ£o encontrado' });
     }
 
-    const mediaCorte = rows[0].media_minima;
     res.json({ mediaCorte });
   } catch (err) {
-    console.error('Erro ao consultar o banco de dados:', err);
-    res.status(500).json({ message: 'Erro ao consultar o banco de dados' });
+    res.status(500).json({ message: err.message });
   }
 });
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
-
